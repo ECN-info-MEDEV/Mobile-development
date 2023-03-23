1445fa8145628c05c82e20552a9e72f5c6834e23
Saleh eddine EL FDALI <elfdali31@gmail.com>
2023-03-23 20:52:03 +0100

    send alertes from alert data view

--- app/src/main/java/com/example/android/devmobonboard/AlertesData.java
+++ app/src/main/java/com/example/android/devmobonboard/AlertesData.java
@@ -2,13 +2,31 @@ package com.example.android.devmobonboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AlertesData extends AppCompatActivity {
-
+    public static final String EXTRA_REPLY =
+            "com.example.android.twoactivities.extra.REPLY";
+    private EditText mReply;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alertes_data);
+        mReply = findViewById(R.id.alert_content);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(HomeActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.alert_content);
+        textView.setText(message);
+    }
+    public void returnReply(View view) {
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
     }
 }
